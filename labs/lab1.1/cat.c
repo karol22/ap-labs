278968bd430d30b50b7d93b76eeb2b3b4f16e252
Karol Gutierrez <kjgs.22@gmail.com>
2019-02-12 16:50:39 -0600

    Add lab 1.1

---
+++ labs/lab1.1/cat.c
@@ -0,0 +1,22 @@
+#include <stdio.h>	
+#include <unistd.h>	
+#include <fcntl.h>
+
+int main(int argc, char **argv){
+	int	i;
+    int file;	// declare local variables
+    int ch;
+
+	for (i = 1; i < argc; i++){ //for every paramater we look for a valid file
+		file = open(argv[i], O_RDONLY);
+		if (file < 0){ //if error
+            perror(argv[i]); 
+            continue; 
+        }
+		while (read(file, &ch, 1) == 1){
+            write( STDOUT_FILENO, &ch, 1);//print to the terminal
+        }
+		close(file);
+	}
+    return 0;
+}
