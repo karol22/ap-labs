17a542e5a67cb0abc04ab7f1c32b0bf03e0eca05
Karol Jose Gutierrez Suarez <karol.gutierrez@kueski.com>
2019-03-12 20:06:25 -0600

    Add Lab 2.5

--- labs/lab2.5/crawl3.go
+++ labs/lab2.5/crawl3.go
@@ -16,49 +16,68 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"flag"
 
 	"gopl.io/ch5/links"
 )
+
+type linkElement struct {
+	Depth int
+	Url string
+}
 
 //!+sema
 // tokens is a counting semaphore used to
 // enforce a limit of 20 concurrent requests.
 var tokens = make(chan struct{}, 20)
+var maxDepth int
 
-func crawl(url string) []string {
-	fmt.Println(url)
-	tokens <- struct{}{} // acquire a token
-	list, err := links.Extract(url)
-	<-tokens // release the token
-
+func crawl(current linkElement) []linkElement {
+	fmt.Println("Depth: ", current.Depth, " URL: ", current.Url)
+	if current.Depth >= maxDepth {
+		return nil
+	}
+	tokens <- struct{}{}
+	urls, err := links.Extract(current.Url)
+	<-tokens
 	if err != nil {
 		log.Print(err)
 	}
-	return list
+	elems := []linkElement{}
+	for _, url := range urls{
+		elems = append(elems, linkElement{current.Depth+1, url})
+	}
+	return elems
 }
 
 //!-sema
 
 //!+
 func main() {
-	worklist := make(chan []string)
+	flag.IntVar(&maxDepth, "depth", 0, "Depth to crawl")
+	flag.Parse()
+	worklist := make(chan []linkElement)
 	var n int // number of pending sends to worklist
 
 	// Start with the command-line arguments.
 	n++
-	go func() { worklist <- os.Args[1:] }()
+	go func() {
+		elems := []linkElement{}
+		elems = append(elems, linkElement{0, os.Args[2]})
+		worklist <- elems
+	}()
 
 	// Crawl the web concurrently.
 	seen := make(map[string]bool)
 	for ; n > 0; n-- {
 		list := <-worklist
-		for _, link := range list {
-			if !seen[link] {
-				seen[link] = true
+		for _, elem := range list {
+			if !seen[elem.Url] {
+				seen[elem.Url] = true
 				n++
-				go func(link string) {
-					worklist <- crawl(link)
-				}(link)
+				go func(elem linkElement) {
+					worklist <- crawl(elem)
+				}(elem)
 			}
 		}
 	}
